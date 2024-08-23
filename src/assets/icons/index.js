7b5bfc73873e04678b94d564dfb2601b5c5ab593
Parul Shah <97349441+Parul101shah@users.noreply.github.com>
2024-08-23 18:36:59 +0530

    Add files via upload

---
+++ src/assets/icons/index.js
@@ -0,0 +1,73 @@
+import css from './css.svg'
+import sql from './sql.svg'
+import java from './java.svg'
+import cpp from './cpp.svg'
+import git from './git.svg'
+import linux from './linux.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import arduino from './arduino.svg'
+import motion from './motion.svg'
+import mui from './mui.svg'
+import mysql from './mysql.svg'
+import python from './python.svg'
+import react from './react.svg'
+import wipro from './wipro.svg'
+import vscode from './vscode.svg'
+import tailwindcss from './tailwindcss.svg'
+import udemy from './udemy.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.svg'
+import estate from './estate.svg'
+import weather from './weather.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import lgm from './lgm.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import stocks from './stocks.svg'
+import card from './card.svg'
+import street from './street.svg'
+import leetcode from './leetcode.svg'
+
+export {
+    css,
+    linux,
+    sql,
+    java,
+    git,
+    cpp,
+    github,
+    html,
+    javascript,
+    arduino,
+    motion,
+    mui,
+    mysql,
+    python,
+    card,
+    react,
+    wipro,
+    vscode,
+    leetcode,
+    stocks,
+    tailwindcss,
+    udemy,
+    linkedin,
+    twitter,
+    street,
+    car,
+    estate,
+    weather,
+    snapgram,
+    summiz,
+    lgm,
+    arrow,
+    contact,
+    soundon,
+    soundoff
+}
